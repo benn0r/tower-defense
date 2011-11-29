0990a962c246a8d2fed5c22045f9bc2b892d5b28
Benjamin Conrad <admin@benn0r.ch>
2011-11-29 16:25:57 +0100

    Kommentare

--- src/ch/benn0r/gaming/cannon/test/TestOwner.java
+++ src/ch/benn0r/gaming/cannon/test/TestOwner.java
@@ -2,6 +2,11 @@ package ch.benn0r.gaming.cannon.test;
 
 import ch.benn0r.gaming.cannon.UnitOwner;
 
+/**
+ * Nur zum testen, symbolisiert einen Spieler auf der Karte
+ * 
+ * @author Benjamin 
+ */
 public class TestOwner implements UnitOwner {
 	
 	public String getName() {
