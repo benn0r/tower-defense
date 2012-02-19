a84e730b14ae9cbde9c12567393decab68012007
benn0r <admin@benn0r.ch>
2012-02-19 18:43:24 +0100

    path finding

--- game.js
+++ game.js
@@ -2,158 +2,162 @@ window.onload = (function() {
     var WIDTH = 256;
     var HEIGHT = 256;
     
+    var TARGET = {x: 9, y: 1};
+    var START = {x: 10, y: 11};
+    
+    var map = new Array();
+    
     Crafty.init(WIDTH, HEIGHT);
     
-    Crafty.sprite(16, "terrain.png", {
-        Grass: [0,0],
-        Wall: [1,0],
-        Castle: [2,0],
-        DefaultMob: [3,0],
-        DefaultTower: [4,0],
-        Blood: [5,0]
-    });
-    
-    Crafty.c("Mob", {
-    	_treasure: {},
-    	_dead: false,
+    Crafty.c("Box", {
+    	wall: false,
     	
-    	init: function() {
-    		this.addComponent("2D, Canvas, DefaultMob");
+    	_draw: function(ctx, po) {
+    		var pos = {_x: po._x + 1, _y: po._y + 1, _w: po._w - 2, _h: po._h -2};
+    		
+    		var x = Math.round(pos._x / 16);
+    		var y = Math.round(pos._y / 16);
+    		
+    		if (x == TARGET.x && y == TARGET.y) {
+    			ctx.fillStyle = "#ff0000";
+    		} else if(this.wall) {
+    			ctx.fillStyle = "#000000";
+    			//this.val = 0; //	 u can't walk there
+    		} else {
+    			ctx.fillStyle = "#ffffff";
+    		}
+    		
+            ctx.fillRect(pos._x, pos._y, pos._w, pos._h);
+            
+            ctx.fillStyle = "#1064C2";
     	},
-    	
-    	// called every round
-    	turn: function() {
-    		if (this._dead) {
-    			// we are dead
-    			return;
-    		}
+    	init: function() {
+    		this.addComponent("2D, Canvas, Color, Tween");
     		
-    		// move to treasure with random speed
-    		var x = Crafty.math.randomInt(0, 8);
-    		var y = Crafty.math.randomInt(0, 8);
+    		this.w = 16;
+    		this.h = 16;
     		
-    		if (this.x > this._treasure.x) {
-    			this.x -= x;
-    		} else {
-    			this.x += x;
-    		}
-    		
-    		if (this.y > this._treasure.y) {
-    			this.y -= y;
-    		} else {
-    			this.y += y;
-    		}
-       	},
-    	
-    	treasure: function(x, y) {
-    		this._treasure = {x: x, y: y};
+    		this.bind("Draw", function(obj) {
+                this._draw(obj.ctx, obj.pos);
+            });
     	}
     });
     
-    Crafty.c("Tower", {
-    	_radius: null,
-    	
-    	init: function() {
-    		this.addComponent("2D, Canvas, DefaultTower");
+    Crafty.c("Path", {
+    	_draw: function(ctx, po) {
+    		var pos = {_x: po._x + 1, _y: po._y + 1, _w: po._w - 2, _h: po._h -2};
+    		
+    		ctx.fillStyle = "#666666";
+            ctx.fillRect(pos._x, pos._y, pos._w, pos._h);
     	},
-    	
-    	// called every round
-    	turn: function(mobs) {
+    	init: function() {
+    		this.addComponent("2D, Canvas, Color, Tween");
     		
-    		var limit = 2; // max kills per round
-    		var kills = 0;
+    		this.w = 16;
+    		this.h = 16;
     		
-    		if (!this._radius) {
-    			// radius for canon
-    			this._radius = new Crafty.circle(this.x, this.y, 60);
-    		}
-    		
-    		for (var i = 0; i < mobs.length; i++) {
-    			if (kills >= limit) {
-    				// reached kill limit
-    				return;
-    			}
-    			
-    			if (!mobs[i]._dead &&
-    					this._radius.containsPoint(mobs[i].x, mobs[i].y)) {
-    				
-    				// we killed the mob
-    				mobs[i].addComponent("Blood");
-    				mobs[i].removeComponent("DefaultMob");
-    				mobs[i]._dead = true;
-    				kills++;
-    			}
-    		}
-       	}
+    		this.bind("Draw", function(obj) {
+                this._draw(obj.ctx, obj.pos);
+            });
+    	}
     });
     
-    // grass floor
     for (var i = 0; i < WIDTH / 16; i++) {
     	for (var j = 0; j < HEIGHT / 16; j++) {
-    		Crafty.e("2D, Canvas, Grass").attr({x: i * 16, y: j * 16});
+    		var box = Crafty.e("Box").attr({x: i * 16, y: j * 16}).color("#000000");
+    		
+    		var x = Math.round(box.x / 16);
+    		var y = Math.round(box.y / 16);
+    		
+    		if ((y == 3 && x > 4 && x < 13) || (x == 12 && y > 3 && y < 15)) {
+    			box.wall = true;
+    		}
+    		
+    		if (!map[x]) {
+    			map[x] = new Array();
+    		}
+    		
+    		map[x][y] = box;
         }
     }
     
-    // walls top and bottom
-    for (var i = 0; i < WIDTH / 16; i++) {
-    	Crafty.e("2D, Canvas, Wall").attr({x: i * 16, y: 0});
-    	
-    	if (i < 10 || i > 12) {
-    		Crafty.e("2D, Canvas, Wall").attr({x: i * 16, y: HEIGHT - 16});
-    	}
-    }
+    var tar = Crafty.e("Box").attr({x: TARGET.x * 16, y: TARGET.y * 16}).color("#000000");
+    var pos = Crafty.e("Path").attr({x: START.x * 16, y: START.y * 16}).color("#000000");
+    pos.val = 0;
     
-    // walls left and right
-    for (var i = 0; i < WIDTH / 16; i++) {
-    	Crafty.e("2D, Canvas, Wall").attr({x: 0, y: i * 16});
-    	
-    	if (i < 10 || i > 12) {
-    		Crafty.e("2D, Canvas, Wall").attr({x: WIDTH - 16, y: i * 16});
-    	}
-    }
-    
-    // treasure for mobs
-    Crafty.e("2D, Canvas, Castle").attr({x: 16 * 2, y: 16 * 2});
-    
-    // test towers
-    var tower1 = Crafty.e("2D, Canvas, Tower").attr({x: 16 * 5, y: 16 * 5});
-//    var tower2 = Crafty.e("2D, Canvas, Tower").attr({x: 16 * 5, y: 16 * 6});
-//    var tower3 = Crafty.e("2D, Canvas, Tower").attr({x: 16 * 5, y: 16 * 7});
-    
-    var mobs = new Array();
-    
-    /**
-     * Generate mobs on the spawnpoints
-     */
-    function spawn() {
-    	for (var i = 0; i < 10; i++) {
-        	var mob = Crafty.e("2D, Canvas, Mob").attr({x: WIDTH - 6 * 16 + Crafty.math.randomInt(0, 32), y: HEIGHT - 16});
-        	mob.treasure(16 * 2, 16 * 2);
-        	
-        	mobs.push(mob);
-        	
-        	var mob = Crafty.e("2D, Canvas, Mob").attr({x: WIDTH - 16, y: HEIGHT - 6 * 16 + Crafty.math.randomInt(0, 32)});
-        	mob.treasure(16 * 2, 16 * 2);
-        	
-        	mobs.push(mob);
-    	}
-    };
-       
-    
-    // calls the turn method of all units on the map
-    setInterval(function() {
-    	for (var i = 0; i < mobs.length; i++) {
-    		mobs[i].turn();
-    	}
-    	
-    	tower1.turn(mobs);
-//    	tower2.turn(mobs);
-//    	tower3.turn(mobs);
-    }, 100);
-    
-    // start with spawning
-    setTimeout(function() {
-    	spawn();
-    }, 1000);
+    var tarp = {x: Math.round(tar.x / 16), y: Math.round(tar.y / 16)};
+	var posp = {x: Math.round(pos.x / 16), y: Math.round(pos.y / 16)};
+	
+	game={};
+	findPath(posp.x, posp.y, tarp.x, tarp.y);
+	
+	var i = game.path.length - 1;
+	setInterval(function() {
+		if (i >= 0) {
+			var wp = game.path[i];
+			
+			pos.x = wp.x * 16;
+			pos.y = wp.y * 16;
+			
+			i--;
+		}
+	}, 200);
+	
+	
+	/**
+	 * Source: http://www.tonypa.pri.ee/tbw/tut22.html
+	 */
+	function findPath(startx, starty, targetx, targety){
+	  path={};
+	  path.Unchecked_Neighbours=[];
+	  path.done = false;
+	  path.name="node_"+starty+"_"+startx;
+	  path[path.name]={x:startx, y:starty, visited:true, parentx:null, parenty:null};
+	  path.Unchecked_Neighbours[path.Unchecked_Neighbours.length]=path[path.name];
+	  while(path.Unchecked_Neighbours.length>0) {
+	    var N = path.Unchecked_Neighbours.shift();
+	    if (N.x == targetx && N.y == targety) {
+	      make_path(N);
+	      path.done = true;
+	      break;
+	    }else {
+	      N.visited=true;
+	      addNode (N, N.x+1, N.y);
+	      addNode (N, N.x-1, N.y);
+	      addNode (N, N.x, N.y+1);
+	      addNode (N, N.x, N.y-1);
+	    }
+	  }
+	  
+	  if (path.done) {
+	    return true;
+	  }else {
+	    return false;
+	  }
+	}
+	
+	/**
+	 * Source: http://www.tonypa.pri.ee/tbw/tut22.html
+	 */
+	function addNode (ob, x, y){
+	  path.name="node_"+y+"_"+x;
+	  if(map[x] && map[x][y] && !map[x][y].wall) {
+	    if (!path[path.name] || path[path.name].visited != true) {
+	      path[path.name]={x:x, y:y, visited:false, parentx:ob.x, parenty:ob.y};
+	      path.Unchecked_Neighbours[path.Unchecked_Neighbours.length]=path[path.name];
+	    }
+	  }
+	}
+	
+	/**
+	 * Source: http://www.tonypa.pri.ee/tbw/tut22.html
+	 */
+	function make_path(ob){
+	  game.path=[];
+	  while (ob.parentx!=null){
+	    game.path[game.path.length]={x: ob.x, y: ob.y};
+	    ob=path["node_"+ob.parenty+"_"+ob.parentx];
+	  }
+	}
     
 });
