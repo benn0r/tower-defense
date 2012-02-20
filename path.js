e7864fb1b9777bb62d306c15c62a9aa4381f67f3
benn0r <admin@benn0r.ch>
2012-02-20 22:38:13 +0100

    fixed bug with pathfinder

--- path.js
+++ path.js
@@ -2,9 +2,9 @@ function path() {
 	
 	var _data = {
 		unchecked: [], // unchecked tiles
-		names: {}, // name to find tile in array with unchecked tiles
+		names: {},
 		done: false,
-		path: [],
+		path: {},
 		result: []
 	};
 	
@@ -32,18 +32,20 @@ function path() {
 		};
 		
 		var id = this._nextId(); // next free slot in stack
+		var name = this._buildName(starter.x, starter.y);
 		
 		// add startpoint to stack
-		_data.names[this._buildName(starter.x, starter.y)] = _data.path.length;
-		_data.unchecked[id] = starter;
-		_data.path[_data.path.length] = starter;
+		_data.path[name] = starter;
+		_data.unchecked[id] = _data.path[name];
+		_data.names[name] = id;
 		
 		while (_data.unchecked.length > 0) {
+//		for (var i = 0; i < 100000; i++) {
 			var tile = _data.unchecked.shift();
 			
 			if (tile.x == target.x && tile.y == target.y) {
 				// we found the target
-				_data.result = this._makePath(tile);
+				 _data.result = this._makePath(tile);
 				return true;
 			} else {
 				tile.visited = true;
@@ -55,6 +57,8 @@ function path() {
 				this._addNode(tile, tile.x, tile.y - 1);
 			}
 		}
+		
+		console.log(_data);
 		
 		return false;
 	};
@@ -73,19 +77,21 @@ function path() {
 					parenty: parent.y
 			};
 			
+			var slot = this._nextId();
+			
 			// add tile to stack
-			_data.names[id] = _data.path.length;
-			_data.unchecked[this._nextId()] = tile;
-			_data.path[_data.path.length] = tile;
+			_data.path[id] = tile;
+			_data.unchecked[slot] = _data.path[id];
+			_data.names[id] = slot;
 		}
 	};
 	
 	this._makePath = function(tile) {
 		var result = [];
 		
-		while (tile.x != _data.path[0].x || tile.y != _data.path[0].y) {
+		while (tile.parentx != null) {
 			result[result.length] = {x: tile.x, y: tile.y};
-			tile = _data.path[_data.names[this._buildName(tile.parentx, tile.parenty)]];
+			tile = _data.path[this._buildName(tile.parentx, tile.parenty)];
 		}
 		
 		return result;
@@ -106,5 +112,7 @@ pathObj.callback(function(x, y) {
 	return false;
 });
 
+//pathObj.find({x: 1, y: 4}, {x: 11, y: 10});
 pathObj.find({x: 1, y: 4}, {x: 11, y: 13});
+
 console.log(pathObj.getPath());
