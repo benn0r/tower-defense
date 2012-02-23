bcee1f430c6545b7fd04a0c24c522efb5fcd093d
Benjamin Conrad <admin@benn0r.ch>
2012-02-23 10:12:48 +0100

    multiple mobs and towers

--- game.js
+++ game.js
@@ -39,7 +39,9 @@ window.onload = (function() {
     	Grass: [0,0],
     	Wall: [1,0],
     	Target: [2,0],
-    	Zombie: [3,0]
+    	Zombie: [3,0],
+    	Defaulttower: [4,0],
+    	Blood: [5,0]
     });
     
     Crafty.c("Terrain", {
@@ -55,6 +57,7 @@ window.onload = (function() {
     	_target: null,
     	_path: [],
     	_wp: null,
+    	killed: false,
     	
     	setTarget: function(target) {
     		_target = target;
@@ -70,6 +73,10 @@ window.onload = (function() {
     		this.h = 16;
     		
     		this.bind("EnterFrame", function() {
+    			if (this.killed) {
+    				return;
+    			}
+    			
     			if (this._path.length > 0 && this._wp == null) {
     				this._wp = this._path.pop();
     				
@@ -94,124 +101,44 @@ window.onload = (function() {
     	}
     });
     
-//    Crafty.c("Box", {
-//    	wall: false,
-//    	
-//    	_draw: function(ctx, po) {
-//    		var pos = {_x: po._x + 1, _y: po._y + 1, _w: po._w - 2, _h: po._h -2};
-//    		
-//    		var x = Math.round(pos._x / 16);
-//    		var y = Math.round(pos._y / 16);
-//    		
-//    		if (x == TARGET.x && y == TARGET.y) {
-//    			ctx.fillStyle = "#ff0000";
-//    		} else if(this.wall) {
-//    			ctx.fillStyle = "#000000";
-//    			//this.val = 0; //	 u can't walk there
-//    		} else {
-//    			ctx.fillStyle = "#ffffff";
-//    		}
-//    		
-//            ctx.fillRect(pos._x, pos._y, pos._w, pos._h);
-//            
-//            ctx.fillStyle = "#1064C2";
-//    	},
-//    	init: function() {
-//    		this.addComponent("2D, Canvas, Color, Tween, Mouse");
-//    		
-//    		this.w = 16;
-//    		this.h = 16;
-//    		
-//    		this.bind("Draw", function(obj) {
-//                this._draw(obj.ctx, obj.pos);
-//            });
-//    		
-//    		this.bind("Click", function(e) {
-//    			if (pos.path.length == 0) {
-//	    			finder.reset();
-//	    			
-//	    			var last = {x: Math.round(pos.x / 16), y: Math.round(pos.y / 16)};
-//	    			
-//	    			finder.find({x: last.x, y: last.y}, {x: Math.round(this.x / 16), y: Math.round(this.y / 16)});
-//	    			var bla = finder.getPath();
-//	    			
-//	    			pos.path = bla;
-//    			}
-//            });
-//    	}
-//    });
-    
-//    Crafty.c("Path", {
-//    	path: [],
-//    	_wp: null,
-//    	
-//    	_draw: function(ctx, po) {
-//    		var pos = {_x: po._x + 1, _y: po._y + 1, _w: po._w - 2, _h: po._h -2};
-//    		
-//    		ctx.fillStyle = "#666666";
-//            ctx.fillRect(pos._x, pos._y, pos._w, pos._h);
-//    	},
-//    	init: function() {
-//    		this.addComponent("2D, Canvas, Color, Tween");
-//    		
-//    		this.w = 16;
-//    		this.h = 16;
-//    		
-//    		this.bind("Draw", function(obj) {
-//                this._draw(obj.ctx, obj.pos);
-//            });
-//    		
-//    		this.bind("EnterFrame", function() {
-//    			
-//    			if (this.path.length > 0 && _wp == null) {
-//    				_wp = this.path.pop();
-//    				
-//    				_wp.x = _wp.x * 16;
-//    				_wp.y = _wp.y * 16;
-//    			}
-//    			
-//    			var x = this.x;
-//    			var y = this.y;
-//    			
-//    			if (_wp && (_wp.x != x || _wp.y != y)) {
-//    				if (_wp.x != x) {
-//    					this.x = (_wp.x < x) ? this.x - 8 : this.x + 8;
-//    				} 
-//    				if (_wp.y != y) {
-//    					this.y = (_wp.y < y) ? this.y - 8 : this.y + 8;
-//    				}
-//    			} else {
-//    				_wp = null;
-//    			}
-//    		});
-//    	}
-//    });
-    
-//    for (var i = 0; i < WIDTH / 16; i++) {
-//    	for (var j = 0; j < HEIGHT / 16; j++) {
-//    		var box = Crafty.e("Box").attr({x: i * 16, y: j * 16}).color("#000000");
-//    		
-//    		var x = Math.round(box.x / 16);
-//    		var y = Math.round(box.y / 16);
-//    		
-//    		if ((y == 3 && x > 4 && x < 17) || 
-//    				(x == 12 && y > 3 && y < 15) ||
-//    				(x == 3 && y > 4 && y < 9) ||
-//    				(x == 6 && y > 0 && y < 6) ||
-//    				(x == 8 && y > 4 && y < 8) ||
-//    				(x == 8 && y > 4 && y < 8) ||
-//    				(y == 10 && x > 2 && x < 12) ||
-//    				(y == 7 && x >= 0 && x < 11)) {
-//    			box.wall = true;
-//    		}
-//    		
-//    		if (!map[x]) {
-//    			map[x] = new Array();
-//    		}
-//    		
-//    		map[x][y] = box;
-//        }
-//    }
+    Crafty.c("Tower", {
+    	_radius: null,
+    	_frame: 0,
+    	
+    	init: function() {
+    		this.addComponent("2D, Canvas, Defaulttower");
+    		
+    		this.bind("EnterFrame", function() {
+    			this._frame++;
+    			
+    			if (this._frame % 60 != 0) {
+    				return;
+    			}
+    			
+    			var limit = 1;
+        		var kills = 0;
+        		
+        		if (!this._radius) {
+        			this._radius = new Crafty.circle(this.x, this.y, 40);
+        		}
+        		
+        		for (var i = 0; i < mobs.length; i++) {
+        			if (kills >= limit) {
+        				return;
+        			}
+        			
+        			if (mobs[i].killed == false &&
+        					this._radius.containsPoint(mobs[i].x, mobs[i].y)) {
+        				console.log("kill");
+        				mobs[i].killed = true;
+        				mobs[i].removeComponent("Zombie");
+        				mobs[i].addComponent("Blood");
+        				kills++;
+        			}
+        		}
+    		});
+    	},
+    });
     
     var target = null;
     
@@ -225,9 +152,11 @@ window.onload = (function() {
     	};
     };
     
+    var mobs = [];
+    
     setInterval(function() {
 	    var paths = [];
-	    var mobs = [];
+	    var mobsstart = mobs.length;
 
 	    for (var mSpawn = 0; mSpawn < _spawns.length; mSpawn++) {
 	    	var zombie = Crafty.e("Mob, Zombie").attr({x: _spawns[mSpawn].x * 16, y: _spawns[mSpawn].y * 16});
@@ -253,10 +182,17 @@ window.onload = (function() {
 	    }
 	    
 	    
-	    for (var i = 0; i < mobs.length; i++) {
+	    for (var i = mobsstart; i < mobs.length; i++) {
 	    	mobs[i].setPath(paths[mobs[i].spawn]);
 	    }
     }, 1000);
+    
+    Crafty.e("Tower").attr({x: 4 * 16, y: 4 * 16});
+    Crafty.e("Tower").attr({x: 8 * 16, y: 4 * 16});
+    Crafty.e("Tower").attr({x: 7 * 16, y: 6 * 16});
+    Crafty.e("Tower").attr({x: 12 * 16, y: 6 * 16});
+    Crafty.e("Tower").attr({x: 12 * 16, y: 4 * 16});
+    Crafty.e("Tower").attr({x: 9 * 16, y: 2 * 16});
     
     
 //    var tar = Crafty.e("Box").attr({x: TARGET.x * 16, y: TARGET.y * 16}).color("#000000");
